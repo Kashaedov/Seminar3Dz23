d6b1dfcd1ed7fe6b3206453fbe9d9a849667f671
Kashaedov Vitaliy <Vitalickashaedov@mail.ru>
2022-07-06 22:26:42 +0300

    zadacha23

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+
+
+void  Zadacha23 (int number1)
+
+{
+    for (int kub = 1; kub <= number1; kub+=1)
+    {
+        Console.Write ($"Число {kub} возведено в куб = ");
+        Console.WriteLine ($"{kub}^3 = {Math.Pow(kub,3)}");
+
+    }   
+}
+Zadacha23 ( number1 );
